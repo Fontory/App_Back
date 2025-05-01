f9522a770309f06c781a62f89fa4dc667a809af3
yoojung <143973878+jungyoojang@users.noreply.github.com>
2025-05-02 03:57:46 +0900

    Update cors

--- src/main/java/com/fontservice/fontory/config/WebConfig.java
+++ src/main/java/com/fontservice/fontory/config/WebConfig.java
@@ -3,6 +3,8 @@ package com.fontservice.fontory.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
+
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -23,6 +25,18 @@ public class WebConfig implements WebMvcConfigurer {
 
         registry.addResourceHandler("/handwriting/**")
                 .addResourceLocations("file:./uploads/handwriting/");
+    }
+
+    // CORS 설정 추가
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 API 경로에 대해
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "http://ceprj.gachon.ac.kr:3000"
+                ) // 프론트 주소
+                .allowedMethods("*") // GET, POST 등 모두 허용
+                .allowCredentials(true);
     }
 
 }
