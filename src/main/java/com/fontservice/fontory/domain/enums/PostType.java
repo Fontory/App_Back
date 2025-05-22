411daf9fa7edfecfde763074a2b3e11ba9813915
yoojung <143973878+jungyoojang@users.noreply.github.com>
2025-05-23 01:21:59 +0900

    update enums

--- src/main/java/com/fontservice/fontory/domain/enums/PostType.java
+++ src/main/java/com/fontservice/fontory/domain/enums/PostType.java
@@ -5,5 +5,15 @@ import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum PostType {
     TRANSCRIPTION,
-    GENERAL
+    GENERAL;
+
+    @JsonValue
+    public String toLower() {
+        return this.name().toLowerCase();
+    }
+
+    @JsonCreator
+    public static PostType from(String value) {
+        return PostType.valueOf(value.toUpperCase());
+    }
 }
